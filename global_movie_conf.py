8f09aed7596cf1064be3c0f47f9a286f32bccc7a
renyx <1079010282@qq.com>
2021-06-22 17:05:31 +0800

    加入前端文件

--- global_movie_conf.py
+++ global_movie_conf.py
@@ -2,3 +2,5 @@ import os
 
 RELATIVE_PATH = os.path.dirname(__file__) + '/'
 data_path = RELATIVE_PATH + 'data/'
+sample_data_dir = os.path.join(os.path.abspath(RELATIVE_PATH), 'data/sample_data')
+generate_data_dir = os.path.join(os.path.abspath(RELATIVE_PATH), 'data/generate_data')
